d07b2e6c6d7c1f7b904616f1e3fb78281f478c01
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2025-01-25 20:26:54 -0500

    Install ESLint

---
+++ 2-module/2.B-lecture/eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPlaywright from 'eslint-plugin-playwright';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'] },
+  { files: ['**/*.ts', '**/*tsx'] },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+  eslintPluginPlaywright.configs['flat/recommended'],
+  {
+    rules: { 'no-console': 'error', 'no-var': 'error' },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
